eeb0c822a68907e3a809bada27f6682fb92ccd14
HARU <emerson.cmelo1@senacsp.edu.br>
2025-02-27 17:06:11 -0300

    tentativa de entender como funciona a calculadora

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("Digite o primeiro número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o operador: ");
+            char operador = Convert.ToChar(Console.ReadLine());
+            switch (operador)
+            {
+                case '+':
+                    Console.WriteLine("O resultado da conta é: " + (num1 + num2));
+                    break;
+                case '-':
+                    Console.WriteLine("O resultado da conta é: " + (num1 - num2));
+                    break;
+                case '*':
+                    Console.WriteLine("O resultado da conta é: " + (num1 * num2));
+                    break;
+                case '/':
+                    Console.WriteLine("O resultado da conta é: " + (num1 / num2));
+                    break;
+            
+
+            }
     }
 }
